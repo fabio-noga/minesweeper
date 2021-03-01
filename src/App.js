e2fc62f69a1a98b1b1a49ae05bcda1b3241050d8
Noga <fabiomiguel98@hotmail.com>
2021-03-01 18:29:31 +0000

    ghpages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Board from './components/Board';
+import { HashRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Minesweeper</h1>
-      <Board/>
-    </div>
+    <HashRouter basename='/'>
+      <div className="App">
+        <h1>Minesweeper</h1>
+        <Board/>
+      </div>
+    </HashRouter>
   );
 }
 
